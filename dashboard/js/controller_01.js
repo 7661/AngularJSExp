b111cbca692cf0b5532017377be6a7111827c276
7661 <tapash_me@yahoo.com>
2015-07-12 12:37:55 -0400

    update navbar

--- dashboard/js/controller_01.js
+++ dashboard/js/controller_01.js
@@ -1,33 +1,11 @@
-myApp.controller('controllerList[0]',['$scope', function($scope) {
-   		$scope.student = {
-      			firstName: "Tapas",
-      			lastName: "",
-      			fullName: function() {
-         		var studentObject;
-         		studentObject = $scope.student;
-         		return studentObject.firstName + " " + studentObject.lastName;
-      			},
-			
-			subjects:[
-         			{name:'Physics',marks:70},
-         			{name:'Chemistry',marks:80},
-         			{name:'Math',marks:65},
-		 		{name:'English',marks:75},
-		 		{name:'Hindi',marks:67}
-      			]
-   		};
-		$scope.buttonText = "Hide";
-		
-		$scope.hide = function(){		
-		$scope.hideName = !$scope.hideName;
-		if($scope.buttonText == "Hide"){$scope.buttonText = "Show";}
-		else{$scope.buttonText = "Show";}		
-		};
-		
-		$scope.moreButton = true;
-		$scope.more = function(){		
-		$scope.moreButton = !$scope.moreButton;		
-		};
-		
-	}]);
+myApp.controller('controllerList[0]', function ($scope) {
+  $scope.phones = [
+    {'name': 'Nexus S',
+     'snippet': 'Fast just got faster with Nexus S.'},
+    {'name': 'Motorola XOOM™ with Wi-Fi',
+     'snippet': 'The Next, Next Generation tablet.'},
+    {'name': 'MOTOROLA XOOM™',
+     'snippet': 'The Next, Next Generation tablet.'}
+  ];
+});
 
